38e0d0b3decb658da0965f43df8c75a7b76a76ef
SaveGex <Alekseew.zheka0207@gmail.com>
2024-05-13 00:15:10 +0300

    Revert "task1"
    This reverts commit 2b6a466868063c02bb6a97763dee60da021ff938.

--- lesson10(10)/lesson10(10).cpp
+++ lesson10(10)/lesson10(10).cpp
@@ -4,56 +4,50 @@
 #include <ctime>
 #include <cstdlib>
 #include <vector>
-#include <algorithm>
 
+<<<<<<< HEAD
 //bonk2 = find(array.begin(), array.end(), bot1);
 //index_bot = distance(array.begin(), bonk2);
 
+=======
+#include <algorithm>
+>>>>>>> 1c2fd62 (Revert "task1")
 using namespace std;
-//vector<int> Sort(vector <int> &arr) {
-//	int quest_sort;
-//	cout << "How you want to sort this array? From lower To Biggest[1] opposite[2]";
-//	cin >> quest_sort;
-//	if (quest_sort == 1) {
-//		sort(arr.begin(), arr.end());
-//		return arr;
-//	}
-//	else{
-//		reverse(arr.begin(), arr.end());
-//		return arr;
-//	}
-//}
-int Max(vector <int> arr, int lenght) {
-	int big;
-	big = arr[0];
-	for (short i = 0; i < lenght; i++) {
-		if (big <= arr[i]) {
-			big = arr[i];
-		}
+vector<int> Sort(vector <int> &arr) {
+	int quest_sort;
+	cout << "How you want to sort this array? From lower To Biggest[1] opposite[2]";
+	cin >> quest_sort;
+	if (quest_sort == 1) {
+		sort(arr.begin(), arr.end());
+		return arr;
 	}
-	return big;
-}
-int Min(vector <int> arr, int lenght) {
-	int low;
-	low = arr[0];
-	for (short i = 0; i < lenght; i++) {
-		if (low >= arr[i]) {
-			low = arr[i];
-		}
+	else{
+		reverse(arr.begin(), arr.end());
+		return arr;
 	}
-	return low;
 }
 int main() {
+<<<<<<< HEAD
 	srand(time(nullptr));
 	int lenght=10;
 	int min, max;
+=======
+	int lenght;
+	int sym;
+
+	cout << "write lenght of array: ";
+	cin >> lenght;
+>>>>>>> 1c2fd62 (Revert "task1")
 	vector <int> array(lenght);
 
+	cout << "Write symbol which be fill array: ";
 	for (short i = 0; i < lenght; i++) {
-		array[i] = rand()%50;
+		cin >> sym;
+		array[i] = sym;
 	}
 	for (auto i : array) {
 		cout << i << ' ';
+<<<<<<< HEAD
 	}
 	cout << endl;
 	min=Min(array, lenght);
@@ -65,6 +59,13 @@ int main() {
 	auto amount1 = find(array.begin(), array.end(), max);
 	int index1 = distance(array.begin(), amount1);
 	cout << "\n" << "max: " << max << '(' << index1+1 << ')';
+=======
+	}
+	array=Sort(array);
+	for (auto i : array) {
+		cout << i<<' ';
+	}
+>>>>>>> 1c2fd62 (Revert "task1")
 
 
 }

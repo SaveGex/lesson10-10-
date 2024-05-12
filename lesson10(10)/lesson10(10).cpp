2b6a466868063c02bb6a97763dee60da021ff938
SaveGex <Alekseew.zheka0207@gmail.com>
2024-05-12 20:41:06 +0300

    task1

--- lesson10(10)/lesson10(10).cpp
+++ lesson10(10)/lesson10(10).cpp
@@ -4,42 +4,57 @@
 #include <ctime>
 #include <cstdlib>
 #include <vector>
-
 #include <algorithm>
+
 using namespace std;
-vector<int> Sort(vector <int> &arr) {
-	int quest_sort;
-	cout << "How you want to sort this array? From lower To Biggest[1] opposite[2]";
-	cin >> quest_sort;
-	if (quest_sort == 1) {
-		sort(arr.begin(), arr.end());
-		return arr;
+//vector<int> Sort(vector <int> &arr) {
+//	int quest_sort;
+//	cout << "How you want to sort this array? From lower To Biggest[1] opposite[2]";
+//	cin >> quest_sort;
+//	if (quest_sort == 1) {
+//		sort(arr.begin(), arr.end());
+//		return arr;
+//	}
+//	else{
+//		reverse(arr.begin(), arr.end());
+//		return arr;
+//	}
+//}
+int Max(vector <int> arr, int lenght) {
+	int big;
+	big = arr[0];
+	for (short i = 0; i < lenght; i++) {
+		if (big <= arr[i]) {
+			big = arr[i];
+		}
 	}
-	else{
-		reverse(arr.begin(), arr.end());
-		return arr;
+	return big;
+}
+int Min(vector <int> arr, int lenght) {
+	int low;
+	low = arr[0];
+	for (short i = 0; i < lenght; i++) {
+		if (low >= arr[i]) {
+			low = arr[i];
+		}
 	}
+	return low;
 }
 int main() {
-	int lenght;
-	int sym;
-
-	cout << "write lenght of array: ";
-	cin >> lenght;
+	srand(time(nullptr));
+	int lenght=3;
+	int min, max;
 	vector <int> array(lenght);
 
-	cout << "Write symbol which be fill array: ";
 	for (short i = 0; i < lenght; i++) {
-		cin >> sym;
-		array[i] = sym;
+		array[i] = rand()%50;
 	}
 	for (auto i : array) {
-		cout << i << ' ';
+		cout << "\n" << i << ' ';
 	}
-	array=Sort(array);
-	for (auto i : array) {
-		cout << i<<' ';
-	}
+	min=Min(array, lenght);
+	max = Max(array, lenght);
+	cout << "\n" << "min: " << min << "\n" << "max: " << max;
 
 
 }

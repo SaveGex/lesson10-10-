64a641c371c3fa485bfae26aa765b09972dda8e9
SaveGex <Alekseew.zheka0207@gmail.com>
2024-05-12 17:36:56 +0300

    Добавьте файлы проекта.

---
+++ lesson10(10)/lesson10(10).cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <Windows.h>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+
+#include <algorithm>
+using namespace std;
+void Paint(string str){
+	SetConsoleCP(1251); 
+	SetConsoleOutputCP(1251);
+	int quest_way, lenght;
+	cout << "write way row[1], col[2]";
+	
+	cin >> quest_way;
+	if (quest_way == 1) {
+		cout << "write lenght";
+		cin >> lenght;
+		for (int i = 0; i < lenght; i++) {
+			cout << str;
+		}
+	}
+	else {
+		cout << "write lenght";
+		cin >> lenght;
+		for (int i = 0; i < lenght; i++) {
+			cout << str<<"\n";
+		}
+	}
+}
+int main() {
+	string str;
+	cin >> str;
+	Paint(str);
+}

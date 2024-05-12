540bd7c88d160b885ef6d5c057170882fdd295e1
SaveGex <Alekseew.zheka0207@gmail.com>
2024-05-12 20:11:29 +0300

    CW

--- lesson10(10)/lesson10(10).cpp
+++ lesson10(10)/lesson10(10).cpp
@@ -7,30 +7,39 @@
 
 #include <algorithm>
 using namespace std;
-void Paint(string str){
-	SetConsoleCP(1251); 
-	SetConsoleOutputCP(1251);
-	int quest_way, lenght;
-	cout << "write way row[1], col[2]";
-	
-	cin >> quest_way;
-	if (quest_way == 1) {
-		cout << "write lenght";
-		cin >> lenght;
-		for (int i = 0; i < lenght; i++) {
-			cout << str;
-		}
+vector<int> Sort(vector <int> &arr) {
+	int quest_sort;
+	cout << "How you want to sort this array? From lower To Biggest[1] opposite[2]";
+	cin >> quest_sort;
+	if (quest_sort == 1) {
+		sort(arr.begin(), arr.end());
+		return arr;
 	}
-	else {
-		cout << "write lenght";
-		cin >> lenght;
-		for (int i = 0; i < lenght; i++) {
-			cout << str<<"\n";
-		}
+	else{
+		reverse(arr.begin(), arr.end());
+		return arr;
 	}
 }
 int main() {
-	string str;
-	cin >> str;
-	Paint(str);
+	int lenght;
+	int sym;
+
+	cout << "write lenght of array: ";
+	cin >> lenght;
+	vector <int> array(lenght);
+
+	cout << "Write symbol which be fill array: ";
+	for (short i = 0; i < lenght; i++) {
+		cin >> sym;
+		array[i] = sym;
+	}
+	for (auto i : array) {
+		cout << i << ' ';
+	}
+	array=Sort(array);
+	for (auto i : array) {
+		cout << i<<' ';
+	}
+
+
 }

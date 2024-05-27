98c0acf0da3d108a066b0301836ad46c1f61138b
SaveGex <Alekseew.zheka0207@gmail.com>
2024-05-27 18:52:23 +0300

    TASK4

--- lesson10(10)/lesson10(10).cpp
+++ lesson10(10)/lesson10(10).cpp
@@ -11,17 +11,55 @@
 
 using namespace std;
 
-int find_max(int digit, int digit2) {
-	return digit > digit2 ? digit : digit2;
-}
-short find_max(short digit, short digit2, short digit3) {
-	return digit > digit2 && digit > digit3 ? digit : digit2 > digit && digit2 > digit3 ? digit2 : digit3;
-}
+int find_position(vector<int>& array) {
+	vector<int> amount_ten;
+	int min_digit, index;
+	for (int i = 0; i < array.size()/10; i++) {
+		int result = 0;
 
+		for (int j = 0; j < array.size(); j++) {
+
+			result += array[j];
+			if ((j % 10 == 0 || j==99) && j != 0) {
+				amount_ten.push_back(result);
+				result = 0;
+			}
+		}
+	}
+	min_digit = amount_ten[0];
+	index = 0;
+	for (int i = 0; i < amount_ten.size(); i++) {
+		if (amount_ten[i] < min_digit) {
+			index = i;
+			min_digit = amount_ten[i];
+		}
+	}
+	sort(amount_ten.begin(), amount_ten.end());
+	for (auto i : amount_ten) {
+		cout << i << ' ';
+	}
+	cout << endl;
+	return index;
+}
 
 int main() {
-	cout << find_max(12, 13) << endl;
-	cout << find_max(2, 52, 10);
+	srand(time(nullptr));
+	vector <int> array;
+	for (int i = 0; i < 100; i++) {
+		array.push_back(0 + rand() % (999 - 0 + 1));
+	}
+	for (auto i : array) {
+		if (i < 10) {
+			cout << ' ';
+		}
+		if (i < 100) {
+			cout << ' ';
+		}
+		cout << i << ' ';
+	}
+	cout << endl;
+	cout << find_position(array);
+
 
 
 

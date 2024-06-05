6b1f349ea84fcb10536f079e88adaacf7869e4b2
SaveGex <Alekseew.zheka0207@gmail.com>
2024-06-05 16:21:45 +0300

    HW1

--- lesson10(10)/lesson10(10).cpp
+++ lesson10(10)/lesson10(10).cpp
@@ -11,54 +11,37 @@
 
 using namespace std;
 
-const int size = 10;
-
-
-vector <int> Join(vector <int> *array, ...) {
-	vector <int> concatenated_array;
-	va_list args;
-	va_start(args, array);
-	for (int i = 0; i < ::size; i++) {
-		int digit = va_arg(args, int);
-		concatenated_array.push_back(digit);
+void Reversee(vector <int> &array, int &size) {
+	int* arr = new int[size];
+	for (int i = 0; i < size; i++) {
+		arr[i] = array[size - i - 1];
 	}
-	va_end(args);
-	return concatenated_array;
+	for (int i = 0; i < size; i++) {
+		array[i] = arr[i];
+	}
+	delete[] arr;
 }
 
 int main() {
 	srand(time(nullptr));
 	vector <int> array;
-	vector <int> array2;
-	vector <int> concatenated_array;
+	int size = 10;
 
-	for (int i = 0; i < 10; i++) {
-		array.push_back(0 + rand() % (99 + 0 - 1));
-		array2.push_back(0 + rand() % (99 + 0 - 1));
+	for (int i = 0; i < size; i++) {
+		array.push_back(rand() % 99);
+	}
+	for (int i = 0; i<array.size(); i++) {
+		if (array[i] < 10)	cout << ' ';
+		cout << ' ' << array[i];
 	}
 	cout << endl;
-	for (auto i : array) {
-		if (i < 10)	cout << ' ';
-		cout << i << ' ';
-	}
-	cout << endl;
-
-	for (auto i : array2) {
-		if (i < 10)	cout << ' ';
-		cout << i << ' ';
-	}
-	cout << endl;
-
-	concatenated_array = Join(&array, &array2);
-
-	for (auto i : concatenated_array) {
-		if (i < 10)	cout << ' ';
-		cout << i << ' ';
+	Reversee(array, size);
+	for (int i = 0; i < array.size(); i++) {
+		if (array[i] < 10)	cout << ' ';
+		cout << ' ' << array[i];
 	}
 
 
 
-
-
-	return 0;
+	return 0; 
 }

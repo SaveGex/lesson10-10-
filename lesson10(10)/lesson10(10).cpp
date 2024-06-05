1bbcc42b98bfdb90f3cda6574298bd997664c298
SaveGex <Alekseew.zheka0207@gmail.com>
2024-06-05 20:46:43 +0300

    HW3

--- lesson10(10)/lesson10(10).cpp
+++ lesson10(10)/lesson10(10).cpp
@@ -5,44 +5,108 @@
 #include <cstdlib>
 #include <algorithm>
 #include <string>
+#include <cctype>
+
 
 //bonk2 = find(array.begin(), array.end(), bot1);
 //index_bot = distance(array.begin(), bonk2);
 
 using namespace std;
 
-void Reversee(int *array, int& size) {
-	int* arr = new int[size];
-	for (int i = 0; i < size; i++) {
-		arr[i] = *(array + size - i - 1);
+vector <int> Count_Digit(int* array, int* array2, int& size, int& size2) {
+	vector <int> vector_array;
+	for(int i=0; i<size; i++){
+
+		bool ticket = true;
+
+		for (int j = 0; j < size; j++) {
+			if (j != size - 1 && j == i)	j++;
+			if (j == size-1)	break;
+			if (*(array + i) == *(array + j)) {
+				ticket = false;
+				break;
+			}
+		}
+		if (ticket) {
+			for (int j = 0; j < size2; j++) {
+				if (*(array + i) == *(array2 + j)) {
+					ticket = false;
+					break;
+				}
+			}
+		}
+		if (ticket) {
+			vector_array.push_back(*(array + i));
+		}
 	}
-	for (int i = 0; i < size; i++) {
-		*(array+i) = arr[i];
+	
+
+	return vector_array;
+}
+
+void Make_array(int *array, int *array2, int& size, int& size2) {
+	vector <int> vector_array;
+	int index = 0;
+	int counter = 0;
+	vector_array = Count_Digit(array, array2, size, size2);
+	counter = vector_array.size();
+	int* array3 = new int[counter];
+
+	cout << endl;
+
+	for (int i = 0; i < counter; i++) {
+		*(array3 + i) = vector_array[i];
+		cout << *(array3 + i) << ' ';
+		if (*(array3 + i) < 10) {
+			cout << ' ';
+		}
+
 	}
-	delete[] arr;
+	
+
+	delete[] array3;
 }
 
 int main() {
 	srand(time(nullptr));
-	int size = 10;
-	int* array = new int[size];
+	int size, size2;
+
+	cout << "Write first amount: ";
+	cin >> size;
+
+	int *array = new int[size];
+
+	cout << "Write second amount: ";
+	cin >> size2;
+	int* array2 = new int[size2];
 
 	for (int i = 0; i < size; i++) {
-		array[i] = rand() % 99;
+		array[i] = rand() % 20;
 	}
-	for (int i = 0; i<size; i++) {
-		if (array[i] < 10)	cout << ' ';
-		cout << ' ' << array[i];
+	for (int i = 0; i < size2; i++) {
+		array2[i] = rand() % 10;
 	}
 	cout << endl;
-	Reversee(array, size);
+
 	for (int i = 0; i < size; i++) {
-		if (array[i] < 10)	cout << ' ';
-		cout << ' ' << array[i];
+		cout << array[i] << ' ';
+		if (array[i] < 10)		cout << ' ';
+	}	
+	cout << endl;
+	for (int i = 0; i < size2; i++) {
+		cout << array2[i] << ' ';
+		if (array2[i] < 10)		cout << ' ';
 	}
 
-	delete[] array;
 
+
+	Make_array(array, array2, size, size2);
+
+
+
+
+	delete[] array;
+	delete[] array2;
 
 	return 0; 
 }

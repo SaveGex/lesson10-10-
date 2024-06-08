e7cede9c0810bbc5ffaa634c2d89826cd4ec6d4e
SaveGex <Alekseew.zheka0207@gmail.com>
2024-06-08 17:07:16 +0300

    HomeWork4(Переплутав завдання 3 та 4. Тому четверте - третє, а трєте - четверте) HW4 зайве

--- lesson10(10)/lesson10(10).cpp
+++ lesson10(10)/lesson10(10).cpp
@@ -1,57 +1,4 @@
-﻿int main() {
-	srand(time(nullptr));
-	int size, size2;
-	cin >> quest_sort;
-	if (quest_sort == 1) {
-		sort(arr.begin(), arr.end());
-		return arr;
-	}
-	else{
-		reverse(arr.begin(), arr.end());
-		return arr;
-	}
-}
-<<<<<<< HEAD
-	srand(time(nullptr));
-	int lenght=10;
-	int min, max;
-=======
-	int lenght;
-	int sym;
-	cin >> quest_sort;
-	if (quest_sort == 1) {
-		sort(arr.begin(), arr.end());
-		return arr;
-	}
-	else{
-		reverse(arr.begin(), arr.end());
-		return arr;
-	}
-}
-<<<<<<< HEAD
-	srand(time(nullptr));
-	int lenght=10;
-	int min, max;
-=======
-	int lenght;
-	int sym;
-	cin >> quest_sort;
-	if (quest_sort == 1) {
-		sort(arr.begin(), arr.end());
-		return arr;
-	}
-	else{
-		reverse(arr.begin(), arr.end());
-		return arr;
-	}
-}
-<<<<<<< HEAD
-	srand(time(nullptr));
-	int lenght=10;
-	int min, max;
-=======
-	int lenght;
-	int sym;
+﻿#include <iostream>
 #include <Windows.h>
 #include <ctime>
 #include <vector>
@@ -60,6 +7,69 @@
 #include <string>
 #include <cctype>
 
+
+//bonk2 = find(array.begin(), array.end(), bot1);
+//index_bot = distance(array.begin(), bonk2);
+
+using namespace std;
+
+vector <int> Count_Digit(int* array, int* array2, int& size, int& size2) {
+	vector <int> vector_array;
+	for (int i = 0; i < size; i++) {
+
+		bool ticket = true;
+
+		for (int j = 0; j < size; j++) {
+			if (j != size - 1 && j == i)	j++;
+			if (j == size - 1)	break;
+			if (*(array + i) == *(array + j)) {
+				ticket = false;
+				break;
+			}
+		}
+		if (ticket) {
+			for (int j = 0; j < size2; j++) {
+				if (*(array + i) == *(array2 + j)) {
+					ticket = false;
+					break;
+				}
+			}
+		}
+		if (ticket) {
+			vector_array.push_back(*(array + i));
+		}
+	}
+
+
+	return vector_array;
+}
+
+void Make_array(int* array, int* array2, int& size, int& size2) {
+	vector <int> vector_array;
+	int index = 0;
+	int counter = 0;
+	vector_array = Count_Digit(array, array2, size, size2);
+	counter = vector_array.size();
+	int* array3 = new int[counter];
+
+	cout << endl;
+
+	for (int i = 0; i < counter; i++) {
+		*(array3 + i) = vector_array[i];
+		cout << *(array3 + i) << ' ';
+		if (*(array3 + i) < 10) {
+			cout << ' ';
+		}
+
+	}
+
+
+	delete[] array3;
+}
+
+int main() {
+	srand(time(nullptr));
+	int size, size2;
 
 	cout << "Write first amount: ";
 	cin >> size;
@@ -72,7 +82,7 @@
 
 	for (int i = 0; i < size; i++) {
 		array[i] = rand() % 20;
-		bool ticket = true;
+	}
 	for (int i = 0; i < size2; i++) {
 		array2[i] = rand() % 10;
 	}
@@ -86,69 +96,9 @@
 	for (int i = 0; i < size2; i++) {
 		cout << array2[i] << ' ';
 		if (array2[i] < 10)		cout << ' ';
-		cout << i << ' ';
-
-
-
-	Make_array(array, array2, size, size2);
-
-
-
 	}
-	delete[] array;
-	delete[] array2;
-	cout << endl;
-	return 0;
-}	max = Max(array, lenght);
-	auto amount = find(array.begin(), array.end(), min);
-	int index = distance(array.begin(), amount);
-	cout << "\n" << "min: " << min << '(' << index+1 << ')';
-	
-	auto amount1 = find(array.begin(), array.end(), max);
-	int index1 = distance(array.begin(), amount1);
-	cout << "\n" << "max: " << max << '(' << index1+1 << ')';
-=======
-	}
-	array=Sort(array);
-	for (auto i : array) {
-		cout << i<<' ';
-		cout << i << ' ';
->>>>>>> 1c2fd62 (Revert "task1")
-	}
-	cout << endl;
-}	auto amount = find(array.begin(), array.end(), min);
-	int index = distance(array.begin(), amount);
-	cout << "\n" << "min: " << min << '(' << index+1 << ')';
-	
-	auto amount1 = find(array.begin(), array.end(), max);
-	int index1 = distance(array.begin(), amount1);
-	cout << "\n" << "max: " << max << '(' << index1+1 << ')';
-=======
-	}
-	array=Sort(array);
-	for (auto i : array) {
-		cout << i<<' ';
-	for (short i = 0; i < lenght; i++) {
->>>>>>> 1c2fd62 (Revert "task1")
-		array[i] = sym;
-		array2[i] = rand() % 10;
-}
-	for (int i = 0; i < size; i++) {
-		cout << array[i] << ' ';
-		if (array[i] < 10)		cout << ' ';
-	}
-	cout << endl;
-	for (int i = 0; i < size2; i++) {
-		cout << array2[i] << ' ';
-		if (array2[i] < 10)		cout << ' ';
-		array2[i] = rand() % 10;
->>>>>>> 1c2fd62 (Revert "task1")
-	cout << endl;
-	delete[] array;
-	delete[] array2;
-		if (array2[i] < 10)		cout << ' ';
-	return 0;
-}
+
+
 
 	Make_array(array, array2, size, size2);
 

b47f85d1ef5faad4b913802d923873d0b0a7b40b
SaveGex <Alekseew.zheka0207@gmail.com>
2024-06-08 17:03:15 +0300

    HW4(Переплутав завдання 3 та 4. Тому четверте - третє, а трєте - четверте)

--- lesson10(10)/lesson10(10).cpp
+++ lesson10(10)/lesson10(10).cpp
@@ -1,4 +1,57 @@
-﻿#include <iostream>
+﻿int main() {
+	srand(time(nullptr));
+	int size, size2;
+	cin >> quest_sort;
+	if (quest_sort == 1) {
+		sort(arr.begin(), arr.end());
+		return arr;
+	}
+	else{
+		reverse(arr.begin(), arr.end());
+		return arr;
+	}
+}
+<<<<<<< HEAD
+	srand(time(nullptr));
+	int lenght=10;
+	int min, max;
+=======
+	int lenght;
+	int sym;
+	cin >> quest_sort;
+	if (quest_sort == 1) {
+		sort(arr.begin(), arr.end());
+		return arr;
+	}
+	else{
+		reverse(arr.begin(), arr.end());
+		return arr;
+	}
+}
+<<<<<<< HEAD
+	srand(time(nullptr));
+	int lenght=10;
+	int min, max;
+=======
+	int lenght;
+	int sym;
+	cin >> quest_sort;
+	if (quest_sort == 1) {
+		sort(arr.begin(), arr.end());
+		return arr;
+	}
+	else{
+		reverse(arr.begin(), arr.end());
+		return arr;
+	}
+}
+<<<<<<< HEAD
+	srand(time(nullptr));
+	int lenght=10;
+	int min, max;
+=======
+	int lenght;
+	int sym;
 #include <Windows.h>
 #include <ctime>
 #include <vector>
@@ -8,46 +61,102 @@
 #include <cctype>
 
 
-//bonk2 = find(array.begin(), array.end(), bot1);
-//index_bot = distance(array.begin(), bonk2);
+	cout << "Write first amount: ";
+	cin >> size;
 
-using namespace std;
+	int* array = new int[size];
+
+	cout << "Write second amount: ";
+	cin >> size2;
+	int* array2 = new int[size2];
+
+	for (int i = 0; i < size; i++) {
+		array[i] = rand() % 20;
+		bool ticket = true;
+	for (int i = 0; i < size2; i++) {
+		array2[i] = rand() % 10;
+	}
+	cout << endl;
+
+	for (int i = 0; i < size; i++) {
+		cout << array[i] << ' ';
+		if (array[i] < 10)		cout << ' ';
+	}
+	cout << endl;
+	for (int i = 0; i < size2; i++) {
+		cout << array2[i] << ' ';
+		if (array2[i] < 10)		cout << ' ';
+		cout << i << ' ';
 
 
 
-int main() {
+	Make_array(array, array2, size, size2);
 
 
-	int  m1 = 5, m2 = 5;
-
-	int** pArr = new int* [m1];
-
-	for (int i = 0; i < m1; i++) {
-
-		pArr[i] = new int[m2];
 
 	}
-
-	// Доступ до елементів масиву виконується як звичайно  
-
-	for (int i = 0; i < m1; i++) {
-		for (int a = 0; a < m2; a++) {
-			pArr[i][a] = rand() % 100;
-			cout << ' ' << pArr[i][a];
-			if (pArr[i][a] < 10)	cout << ' ';
-		}
-		cout << endl;
+	delete[] array;
+	delete[] array2;
+	cout << endl;
+	return 0;
+}	max = Max(array, lenght);
+	auto amount = find(array.begin(), array.end(), min);
+	int index = distance(array.begin(), amount);
+	cout << "\n" << "min: " << min << '(' << index+1 << ')';
+	
+	auto amount1 = find(array.begin(), array.end(), max);
+	int index1 = distance(array.begin(), amount1);
+	cout << "\n" << "max: " << max << '(' << index1+1 << ')';
+=======
 	}
-
-	// Послідовне видалення двовимірного масиву...  
-
-	for (int i = 0; i < m1; i++) {
-
-		delete[]pArr[i];
-
+	array=Sort(array);
+	for (auto i : array) {
+		cout << i<<' ';
+		cout << i << ' ';
+>>>>>>> 1c2fd62 (Revert "task1")
 	}
+	cout << endl;
+}	auto amount = find(array.begin(), array.end(), min);
+	int index = distance(array.begin(), amount);
+	cout << "\n" << "min: " << min << '(' << index+1 << ')';
+	
+	auto amount1 = find(array.begin(), array.end(), max);
+	int index1 = distance(array.begin(), amount1);
+	cout << "\n" << "max: " << max << '(' << index1+1 << ')';
+=======
+	}
+	array=Sort(array);
+	for (auto i : array) {
+		cout << i<<' ';
+	for (short i = 0; i < lenght; i++) {
+>>>>>>> 1c2fd62 (Revert "task1")
+		array[i] = sym;
+		array2[i] = rand() % 10;
+}
+	for (int i = 0; i < size; i++) {
+		cout << array[i] << ' ';
+		if (array[i] < 10)		cout << ' ';
+	}
+	cout << endl;
+	for (int i = 0; i < size2; i++) {
+		cout << array2[i] << ' ';
+		if (array2[i] < 10)		cout << ' ';
+		array2[i] = rand() % 10;
+>>>>>>> 1c2fd62 (Revert "task1")
+	cout << endl;
+	delete[] array;
+	delete[] array2;
+		if (array2[i] < 10)		cout << ' ';
+	return 0;
+}
 
-	delete[]pArr;
+	Make_array(array, array2, size, size2);
 
-	return 0; 
+
+
+
+	delete[] array;
+	delete[] array2;
+
+	return 0;
 }

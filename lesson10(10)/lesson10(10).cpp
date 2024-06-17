b4883b60743920ddbc899dc3051099283802a367
SaveGex <Alekseew.zheka0207@gmail.com>
2024-06-17 14:44:17 +0300

    task1(not ready)

--- lesson10(10)/lesson10(10).cpp
+++ lesson10(10)/lesson10(10).cpp
@@ -11,61 +11,115 @@
 
 using namespace std;
 
-int size;
-
-int *Return_Pointer(int *array) {
-	int entry_counter = 0;
-
-	for (int i = 0; i < ::size; i++) {
-		if (array[i] % 2 == 0) {
-			entry_counter++;
-		}
-	}
-
-	if (entry_counter > 0) {
-		int index = 0;
-		int* even_array = new int[entry_counter];
-
-		for (int i = 0; i < ::size; i++) {
-			if (array[i] % 2 == 0) {
-				even_array[index] = array[i];
-				index++;
-				
-			}
-		}
-		::size = index;
-		return even_array;
-	}
-
-	::size = 0;
-	return array;
-}
-
-
+void Fill_Array(string digits, vector <int>* array);
+bool Check_Correct_Input(string check);
+int Infinite_Question(string check);
 
 int main() {
 	srand(time(nullptr));
 
-	int size, size2;
+	int check_int, index;
+	bool ticket = true; //цикл
+	string digits; 
+	vector <int> array;
 
-	cout << "Write size of array: ";
-	cin >> size;
-	::size = size;
 
-	int* array = new int[size];
+	while (ticket) {
 
-	for (int i = 0; i < size; i++) {
-		array[i] = rand() % 99;
+		cout << "write your digits: ";
+		getline(cin, digits);
+		Fill_Array(digits, &array);
+
+		cout << "\nComplete add? \n[1]Yes [2]No: ";
+		string check;
+		getline(cin, check);
+		bool ticket_correct = Check_Correct_Input(check);
+
+		if (!ticket_correct) {
+			 check_int = Infinite_Question(check);
+		}
+		else {
+			check_int = stoi(check);
+		}
+		if (check_int == 1) {
+			ticket = false;
+		}
+
 	}
+
+	cout << endl;
+
+	for (int i = 0; i < array.size(); i++) {
+		cout << array[i] << ' ';
+	}
+
+	/*cout << "Which numbers you want to move: ";
+	getline(cin, digits);*/
+
+
 	
-	int *array2 = Return_Pointer(array);
 
 
-	for (int i = 0; i < ::size; i++) {
-		cout << array2[i] << ' ';
-	}
-
-	cout << endl << array2;
 
 	return 0;
+}
+
+
+int Infinite_Question(string check) {
+	int digit;
+	string result;
+	for (int i = 0; i < check.length() + 1; i++) {
+		if (isdigit(check[i])) {
+			result += check[i];
+		}
+	}
+
+	digit = stoi(result);
+	result = "";
+
+	while (digit < 1 && digit > 2) {
+
+		cout << "\n\033[31mComplete add?\033[0m \n[1]Yes [2]No: ";
+		getline(cin, check);
+		for (int i = 0; i < check.length() + 1; i++) {
+			if (isdigit(check[i])) {
+				result += check[i];
+			}
+		}
+		digit = stoi(result);
+		result = "";
+		cin.ignore();
+	}
+
+	return digit;
+}
+
+
+
+
+bool Check_Correct_Input(string check) {
+	bool ticket_correct = true;
+	for (int i = 0; i < check.length() + 1; i++) {
+		if (!isdigit(check[i])) {
+			ticket_correct = false;
+			break;
+		}
+	}
+	return ticket_correct;
+}
+
+
+
+
+void Fill_Array(string digits, vector <int>* array){
+	string result;
+	for (int i = 0; i <	digits.length() + 1; i++) {
+		if (isdigit(digits[i])) {
+			result += digits[i];
+		}
+		else if(digits[i] == ' ') {
+			(*array).push_back(stoi(result));
+			result = "";
+		}
+	}
 }

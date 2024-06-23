7c939359ca294599a6618a0a1565077f2512001c
SaveGex <Alekseew.zheka0207@gmail.com>
2024-06-23 20:19:15 +0300

    P2 Task4

--- lesson10(10)/lesson10(10).cpp
+++ lesson10(10)/lesson10(10).cpp
@@ -7,27 +7,34 @@
 
 using namespace std;
 
+bool Step_by_Step(int counter, int counter_end, char sentence[]);
+
 int main() {
 	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
 
-	char sentence[] = "About No description, website, or topics provided.";
-	char *ptr = sentence;
-	int words = 0;
-	bool ticket = false;
-	while (*ptr != '\0') {
-		if (isalpha(*ptr)) {
-			ticket = true;
+	char sentence[] = "TIKKIT";
+	bool ticket = true;
+	int counter = 0, counter_from_end = strlen(sentence)-1;
+	
+	for (int i = 0; i<strlen(sentence)-1; i++) {
+		ticket = Step_by_Step(counter, counter_from_end, sentence);
+		if (!ticket) {
+			cout << "Word not is palindrome";
+			return 0;
 		}
-		if (ticket && (*ptr == ',' || *ptr == ' ' || *ptr == '.')) {//(isalpha(static_cast<unsigned char>(*ptr))) НЕ ПРАЦЮЄ
-			words++;
-			ticket = false;
-		}
-		ptr++;
+		counter++;
+		counter_from_end--;
 	}
-	cout << "Letters: " << words;
+	cout << "Word is palindrome";
 	return 0;
 }
 
 
+bool Step_by_Step(int counter, int counter_end, char sentence[]) {
+	if (sentence[counter] != sentence[counter_end]) {
+		return false;
+	}
+	return true;
+}
 
